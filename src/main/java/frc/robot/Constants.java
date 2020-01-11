98f52c7e1ed242fb963895e981b50e2c70015e4b
Jamesni <2667151484@qq.com>
2020-01-11 10:58:21 -0500

    init code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,4 +1,12 @@
 package frc.robot;
 public final class Constants{
+    //Drive train motor
+    public int LeftMaster = 1;
+    public int LeftMotor1 = 2;
+    public int LeftMotor2 = 3;
+    public int RightMaster = 4;
+    public int RightMotor1 = 5;
+    public int RightMotor2 = 6;
+    
     
 }
