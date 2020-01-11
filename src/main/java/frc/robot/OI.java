211ffcf0f125b3ace67a2deb1bfe77ac6bd2addf
Jamesni <2667151484@qq.com>
2020-01-11 10:30:54 -0500

    init code

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,4 +1,11 @@
 package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
 public class OI{
-    
+    public void driver_Control(Joystick driver_Control){
+
+    }
+    public void operator_Control(Joystick operator_Control){
+
+    }
 }
