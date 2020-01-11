6227dc3c7b64e9121fbb89f7d3390307ad570c69
Jamesni <2667151484@qq.com>
2020-01-11 10:51:42 -0500

    init code

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -3,11 +3,13 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 public class OI{
     public OI(){
-        
+
     }
+    //Driver control
     public void driver_Control(Joystick driver_Control){
 
     }
+    //Operator control
     public void operator_Control(Joystick operator_Control){
 
     }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,28 +1,12 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.Camera.Camera;
 import frc.robot.OI;
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
+
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used
-   * for any initialization code.
-   */
+ 
   private Camera camera;
   private Joystick driver_Control;
   private Joystick operator_Control;
@@ -42,8 +26,10 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     oi = new OI();
+    //init Driver and Oerator Joystick
     driver_Control = new Joystick(0);
     operator_Control = new Joystick(1);
+
     camera = new Camera(0);
   }
 
