652be5b88505d21baeee78d82e161183679d8809
Jamesni <2667151484@qq.com>
2020-01-11 21:28:24 -0500

    add joystick and motor

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,12 +1,18 @@
 package frc.robot;
 public final class Constants{
+    //Joystick
+    public static int driver_Control = 0;
+    public static int operator_Control = 0;
     //Drive train motor
-    public static int LeftMaster = 1;
-    public static int LeftMotor1 = 2;
-    public static int LeftMotor2 = 3;
-    public static int RightMaster = 4;
-    public static int RightMotor1 = 5;
-    public static int RightMotor2 = 6;
+    public static int mLeftMaster = 1;
+    public static int mLeftMotor1 = 2;
+    public static int mLeftMotor2 = 3;
+    public static int mRightMaster = 4;
+    public static int mRightMotor1 = 5;
+    public static int mRightMotor2 = 6;
+    //Intake motor
+    //Climb motor
+    //Shooter motor
     //camera
     public static int cWidth = 480;
     public static int cHeight = 640;
