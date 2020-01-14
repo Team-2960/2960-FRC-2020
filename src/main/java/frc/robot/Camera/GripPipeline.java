dbb4213ca50dd45bba33fdc0388525953db09a87
jrabps <jra4@bps-schools.com>
2020-01-14 16:00:13 -0500

    Before Camera Changes

--- src/main/java/frc/robot/Camera/GripPipeline.java
+++ src/main/java/frc/robot/Camera/GripPipeline.java
@@ -73,7 +73,7 @@ public class GripPipeline implements VisionPipeline {
 
 		// Step Filter_Contours0:
 		ArrayList<MatOfPoint> filterContoursContours = findContoursOutput;
-		double filterContoursMinArea = 150.0;
+		double filterContoursMinArea = 10.0;
 		double filterContoursMinPerimeter = 0;
 		double filterContoursMinWidth = 0;
 		double filterContoursMaxWidth = 1000;
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -30,19 +30,19 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    oi = new OI();
+    //oi = new OI();
     leds = new LEDs();
     //init Driver and Oerator Joystick
     driver_Control = new Joystick(Constants.driver_Control);
     operator_Control = new Joystick(Constants.operator_Control);
     //SmartDashboard.putNumber("CenterX", camera.getImageResultsX());
     //SmartDashboard.putNumber("CenterY", camera.getImageResultsY());
-    SmartDashboard.putNumber("Constants.hsvThresholdHueMin", Constants.hsvThresholdHue[0]);
-    SmartDashboard.putNumber("Constants.hsvThresholdHueMax", Constants.hsvThresholdHue[1]);
-    SmartDashboard.putNumber("Constants.hsvThresholdSaturationMin", Constants.hsvThresholdSaturation[0]);
-    SmartDashboard.putNumber("Constants.hsvThresholdSaturationMax", Constants.hsvThresholdSaturation[1]);
-    SmartDashboard.putNumber("Constants.hsvThresholdValueMin", Constants.hsvThresholdValue[0]);
-    SmartDashboard.putNumber("Constants.hsvThresholdValueMax", Constants.hsvThresholdValue[1]);
+    SmartDashboard.putNumber("Constants.hsvThresholdHueMin", Constants.hsvThresholdHue[1]);
+    SmartDashboard.putNumber("Constants.hsvThresholdHueMax", Constants.hsvThresholdHue[0]);
+    SmartDashboard.putNumber("Constants.hsvThresholdSaturationMin", Constants.hsvThresholdSaturation[1]);
+    SmartDashboard.putNumber("Constants.hsvThresholdSaturationMax", Constants.hsvThresholdSaturation[0]);
+    SmartDashboard.putNumber("Constants.hsvThresholdValueMin", Constants.hsvThresholdValue[1]);
+    SmartDashboard.putNumber("Constants.hsvThresholdValueMax", Constants.hsvThresholdValue[0]);
     
 
     camera = new Camera(0);
@@ -50,16 +50,17 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    oi.driver_Control(driver_Control);
-    oi.operator_Control(operator_Control);
+    //oi.driver_Control(driver_Control);
+    //oi.operator_Control(operator_Control);
+    cameraSetting();
   }
   public void cameraSetting(){
-    Constants.hsvThresholdHue[0] = SmartDashboard.getNumber("HueMin", Constants.hsvThresholdHue[0]);
-    Constants.hsvThresholdHue[1] = SmartDashboard.getNumber("HueMax", Constants.hsvThresholdHue[1]);
-    Constants.hsvThresholdSaturation[0] = SmartDashboard.getNumber("SaturationMin", Constants.hsvThresholdSaturation[0]);
-    Constants.hsvThresholdSaturation[1] = SmartDashboard.getNumber("SaturationMax", Constants.hsvThresholdSaturation[1]);
-    Constants.hsvThresholdValue[0] = SmartDashboard.getNumber("ValueMin", Constants.hsvThresholdValue[0]);
-    Constants.hsvThresholdValue[1] = SmartDashboard.getNumber("ValueMax", Constants.hsvThresholdValue[1]);
+    Constants.hsvThresholdHue[0] = SmartDashboard.getNumber("HueMax", Constants.hsvThresholdHue[0]);
+    Constants.hsvThresholdHue[1] = SmartDashboard.getNumber("HueMin", Constants.hsvThresholdHue[1]);
+    Constants.hsvThresholdSaturation[0] = SmartDashboard.getNumber("SaturationMax", Constants.hsvThresholdSaturation[0]);
+    Constants.hsvThresholdSaturation[1] = SmartDashboard.getNumber("SaturationMin", Constants.hsvThresholdSaturation[1]);
+    Constants.hsvThresholdValue[0] = SmartDashboard.getNumber("ValueMax", Constants.hsvThresholdValue[0]);
+    Constants.hsvThresholdValue[1] = SmartDashboard.getNumber("ValueMin", Constants.hsvThresholdValue[1]);
   }
   @Override
   public void testInit() {
