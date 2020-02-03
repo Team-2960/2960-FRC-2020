a05f54d5e8dd6de8fde2ba9148771b9efe0aa513
Jamesni <2667151484@qq.com>
2020-02-03 09:21:51 -0500

    batter name, add some drive control

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,10 +7,9 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Camera.Camera;
 public class OI{
-    //private Drive drive;
     private Climb climb;
     private Intake intake;
-    
+    private Drive drive;
     public Shooter shooter;
     private Camera camera;
     private double cameraTurn;
@@ -34,51 +33,57 @@ public class OI{
     }
     //Driver control
     public void driver_Control(Joystick driver_Control, Joystick joystick2){
-/*         //drive.move(driver_Control.getRawAxis(1), driver_Control.getRawAxis(5));
+        /*         //drive.move(driver_Control.getRawAxis(1), driver_Control.getRawAxis(5));
+                if(driver_Control.getRawButton(2)){
+                    shooter.setShooterSpeed(driver_Control.getRawAxis(1));
+                }else{
+                    shooter.setShooterSpeed(0);
+                }
+                 */
+                //driver_Control.setRumble(RumbleType.kLeftRumble, 1);
+                //driver_Control.setRumble(RumbleType.kRightRumble, 1);
+        /*         shooter.smart();
+                if(driver_Control.getRawButton(2)){
+                    shooter.setPIDShooterSpeed(1000);
+                }
+                else{
+                    shooter.setPIDShooterSpeed(0);
+                } */
+                
+                
+                
         if(driver_Control.getRawButton(2)){
-            shooter.setShooterSpeed(driver_Control.getRawAxis(1));
-        }else{
-            shooter.setShooterSpeed(0);
-        }
-         */
-        //driver_Control.setRumble(RumbleType.kLeftRumble, 1);
-        //driver_Control.setRumble(RumbleType.kRightRumble, 1);
-/*         shooter.smart();
-        if(driver_Control.getRawButton(2)){
-            shooter.setPIDShooterSpeed(1000);
-        }
-        else{
-            shooter.setPIDShooterSpeed(0);
-        } */
-        
-        
-        
-        /* if(driver_Control.getRawButton(2)){
-           drive.setDrivePID(20);
-        }
-        else if(joystick2.getRawButton(2)){
-            drive.setDriveArcPID(-200 * joystick2.getRawAxis(0), -1 * driver_Control.getRawAxis(1));
-        }
-        else{
-            drive.move((driver_Control.getRawAxis(1)), (joystick2.getRawAxis(1)));
-        } */
-        /*if(driver_Control.getRawAxis(1) == 0 && driver_Control.getRawAxis(5) == 0){
-        if(driver_Control.getRawButton(1)){
-            if(Math.abs(gyro.getAngle()) < 26.4243){
-                driver_Control.setRumble(RumbleType.kRightRumble, 1);
-                driver_Control.setRumble(RumbleType.kLeftRumble, 1);
-            }
-            else{
-                    driver_Control.setRumble(RumbleType.kRightRumble, 0);
-                    driver_Control.setRumble(RumbleType.kLeftRumble, 0);
-            }
-        drive.move(-cameraTurn, cameraTurn);
-        }
-        else{
-            drive.move(0, 0);
-        }
-        }*/
-    
+            drive.setDriveToAngle((camera.calcAngle(camera.getCenterX())), -0.2);
+        } else if (joystick2.getRawButton(2)) {
+            drive.setArcDriveRate(-200 * joystick2.getRawAxis(0), -1 * driver_Control.getRawAxis(1));
+        } else if (driver_Control.getRawButton(1)) {
+            drive.setDriveToAngle(90, 0);
+        } else if (joystick2.getRawButton(1)){
+            drive.setDriveRate(200);
+        } else if (driver_Control.getRawButton(4)){
+            drive.setDriveRate(0);
+        } else if(driver_Control.getRawButton(3)){
+            drive.navXReset();
+        } else{
+            drive.setSpeed((driver_Control.getRawAxis(1)), (joystick2.getRawAxis(1)));
+        } 
+                /* if(driver_Control.getRawAxis(1) == 0 && driver_Control.getRawAxis(5) == 0){
+                if(driver_Control.getRawButton(1)){
+                    if(Math.abs(gyro.getAngle()) < 26.4243){
+                        driver_Control.setRumble(RumbleType.kRightRumble, 1);
+                        driver_Control.setRumble(RumbleType.kLeftRumble, 1);
+                    }
+                    else{
+                            driver_Control.setRumble(RumbleType.kRightRumble, 0);
+                            driver_Control.setRumble(RumbleType.kLeftRumble, 0);
+                    }
+                drive.move(-cameraTurn, cameraTurn);
+                } */
+        /*         else{
+                    drive.move(0, 0);
+                }
+                } */
+            
     }
     //Operator control
     public void operator_Control(Joystick operator_Control){
