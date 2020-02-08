e5723fede0a8d5ae4e43e60d9fb066cc22e65d9c
Jamesni <2667151484@qq.com>
2020-02-08 15:39:32 -0500

    untested auton code

---
+++ src/main/java/frc/robot/Auto/Commands/MoveDistance.java
@@ -0,0 +1,82 @@
+
+package frc.robot.Auto.Commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.SubSystems.Drive;
+
+public class MoveDistance extends CommandGroup{
+
+    Drive drive = Drive.get_Instance();
+    private boolean isFinish = false;
+    private double distance;
+    private double speed;
+    private double angle;
+
+    public MoveDistance(double distance, double angle, double speed){
+        this.distance = distance;
+        this.speed = speed;
+        this.angle = angle;
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        drive.startGoToAngleDistance(speed, angle, distance, 1);
+    }
+
+    /**
+     * Starts up the command. Gets the command ready to start. <p> Note that the command will
+     * eventually start, however it will not necessarily do so immediately, and may in fact be
+     * canceled before initialize is even called. </p>
+     *
+     *
+     */
+    @Override
+    public synchronized void start() {
+        super.start();
+    }
+
+
+    /**
+     * Returns true if all the commands in this group have been started and have
+     * finished.
+     * <p>
+     * <p> Teams may override this method, although they should probably reference super.isFinished()
+     * if they do. </p>
+     *
+     * @return whether this {@link CommandGroup} is finished
+     */
+    @Override
+    protected boolean isFinished() {
+        if(isFinish)
+            return true;
+        else
+            return false;
+    }
+
+
+
+    @Override
+    protected void execute() {
+        isFinish = drive.checkDistance();
+    }
+
+    @Override
+    protected void end() {
+       drive.setSpeed(0,0);
+       drive.disableDrivePID();
+    }
+
+
+
+    /**
+     * Returns whether or not the command is running. This may return true even if the command has
+     * just been canceled, as it may not have yet called
+     *
+     * @return whether or not the command is running
+     */
+    @Override
+    public synchronized boolean isRunning() {
+        return super.isRunning();
+    }
+}
