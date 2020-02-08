3360586b3f46e9ec07f363821b5034634dbd13e7
Jamesni <2667151484@qq.com>
2020-02-08 12:53:06 -0500

    speed controler photoeye

--- src/main/java/frc/robot/SubSystems/Index.java
+++ src/main/java/frc/robot/SubSystems/Index.java
@@ -2,30 +2,38 @@ package frc.robot.SubSystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+
+import com.revrobotics.CANDigitalInput;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Index extends SubsystemBase {
   private static Index index;
-  private static DigitalInput photoeye;
-
+  private CANDigitalInput photoeye;
+  
+  private CANSparkMax mRightIndex;
+  private CANSparkMax mLeftIndex;
   public static Index get_Instance(){
     if(index == null){
       index = new Index();
     } 
     return index;
   }
-  public boolean checkForBall(){
-    return photoeye.get();
-  }
   private Index() {
-    photoeye = new DigitalInput(0);
+    mRightIndex = new CANSparkMax(11, MotorType.kBrushless);
+    photoeye = new CANDigitalInput(mRightIndex,  CANDigitalInput.LimitSwitch.kForward,  CANDigitalInput.LimitSwitchPolarity.kNormallyOpen);
 
+  }
+  public void setSpeed(double speed){
+    System.out.println(photoeye.get());
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-    SmartDashboard.putBoolean("photoeye", photoeye.get());
+    // This method will be called once per scheduler run 
+    System.out.println(photoeye.get());
   }
 }
